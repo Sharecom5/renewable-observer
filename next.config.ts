740e05fa5c13db91404bc5db1aaa7f00925f2734
Your Name <youremail@example.com>
2026-06-27 11:49:05 +0530

    Allow all image domains temporarily

--- next.config.ts
+++ next.config.ts
@@ -5,31 +5,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
+        hostname: '**',
       },
       {
-        protocol: 'https',
-        hostname: 'avatar.vercel.sh',
-      },
-      {
-        protocol: 'https',
-        hostname: 'admin.renewableobserver.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'renewablemirror.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.renewablemirror.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'renewable-mirror.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.renewable-mirror.com',
+        protocol: 'http',
+        hostname: '**',
       }
     ],
   },
